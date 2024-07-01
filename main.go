cbe1a6b0097b2e974d1675ac65b4145b85bbe984
life00 <contact.5lu1v@simplelogin.co>
2024-07-01 19:17:11 +0200

    implement getCfg() and getCjDirs() functions
    Created new get global awecron config and get cronjob directories
    functions.

--- main.go
+++ main.go
@@ -1,18 +1,23 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
+
+	// "os/exec"
+	// "os/user"
 	"path/filepath"
+	"strings"
 
 	"github.com/pelletier/go-toml/v2"
 )
 
-// config of individual cronjobs
+// global awecron config
 var cfg struct {
-	User string
-	Name string
-	Time int
+	Max     int
+	Min     int
+	Timeout int
 }
 
 // minimal error check implementation
@@ -22,27 +27,41 @@ func check(err *error) {
 	}
 }
 
-// running a cronjob
-func run(cronjobPath *string) {
-	// reading config
-	cfgData, err := os.ReadFile(*cronjobPath + "/cfg")
+// gets global awecron configuration
+func getCfg(cfgDir *string) {
+	cfgData, err := os.ReadFile(*cfgDir + "/cfg")
 	check(&err)
-
-	// unmarshal and write config to struct
 	err = toml.Unmarshal(cfgData, &cfg)
 	check(&err)
-	cfgData = nil                          // wipe unneeded variable
-	cfg.Name = filepath.Base(*cronjobPath) // set cfg.Name from the directory name
+}
 
-	log.Print(cfg.Name, " ", cfg.User)
-	// ...
+// gets cronjob directory paths
+func getCjDirs(cfgDir *string) (cjDirs []string) {
+	cjTmrs, err := filepath.Glob(*cfgDir + "/*/tmr")
+	check(&err)
+	// removing the /tmr end
+	for i := 0; i < len(cjTmrs); i++ {
+		cjDirs = append(cjDirs, strings.TrimSuffix(cjTmrs[i], "/tmr"))
+	}
+	return cjDirs
 }
 
 func main() {
-	// TODO: detection of cronjobs and loop
-	cronjobPath := "/tmp/cronjob/"
-	// ...
-	run(&cronjobPath)
-	log.Print("success")
+	// TODO: implement awecron config directory detection
+	cfgDir := "/tmp/awecron"
+
+	// getting global awecron configuration
+	getCfg(&cfgDir)
+
+	// for testing
+	fmt.Print("\nMax: ", cfg.Max, "\nMin: ", cfg.Min, "\nTimeout: ", cfg.Timeout, "\n")
+
+	// getting cronjob directories
+	var cjDirs []string = getCjDirs(&cfgDir)
+
+	// for testing
+	for i := 0; i < len(cjDirs); i++ {
+		fmt.Println(cjDirs[i])
+	}
 	// ...
 }
