f7d5ffd3364eb5a17ac3fe995cafec1de1359b44
life00 <contact.5lu1v@simplelogin.co>
2024-07-03 11:14:02 +0200

    move some runCj code to scheduleCj

--- awecron.go
+++ awecron.go
@@ -55,6 +55,7 @@ func getCjDirs(cfgDir *string) (cjDirs []string) {
 	return cjDirs
 }
 
+// check if its time to run the cronjob
 func checkCj(cjDir *string) bool {
 	// getting last modification date of tmr file
 	cjTmr, err := os.Stat(*cjDir + "/tmr")
@@ -71,6 +72,7 @@ func checkCj(cjDir *string) bool {
 	}
 }
 
+// run the cronjob
 func runCj(cjDir *string) {
 	// used later for logging
 	curUser, _ := user.Current()
@@ -92,43 +94,9 @@ func runCj(cjDir *string) {
 	if err == nil {
 		// log everything
 		log.Printf("awecron info (%s) {%s} [%d]: cronjob run is successful", curUser.Username, path.Base(*cjDir), cjCmd.ProcessState.ExitCode())
-		// TODO: put this all into a new function
-
-		// getting the plain text interval configuration
-		// its also possible to do it with fmt.Fscanf, but I've chosen this option
-		cjCfgData, err := os.ReadFile(*cjDir + "/cfg")
-		if err != nil {
-			log.Printf("awecron error (%s) {%s}: problem reading cronjob config file cjDir/cfg and saving as cronjob config data cjCfgData", curUser.Username, path.Base(*cjDir))
-			return
-		}
-		// conversion
-		cjCfg, err := strconv.Atoi(strings.TrimSpace(string(cjCfgData)))
-		if err != nil {
-			log.Printf("awecron error (%s) {%s}: problem converting cronjob config data cjCfgData into cronjob config integer cjCfg", curUser.Username, path.Base(*cjDir))
-			return
-		}
-		// make sure its greater than zero
-		if cjCfg <= 0 {
-			log.Printf("awecron error (%s) {%s}: cronjob config cjCfg should be greater than zero", curUser.Username, path.Base(*cjDir))
-			return
-		}
-		// create tmr file again
-		cjTmr, err := os.Create(*cjDir + "/tmr")
-		// all fatal errors because I am not risking with tmr file
-		// because it might result in an infinite loop for whatever reason
-		if err != nil {
-			log.Fatalf("awecron fatal (%s) {%s}: problem creating cjDir/tmr file", curUser.Username, path.Base(*cjDir))
-		}
-		// closing cjTmr file
-		err = cjTmr.Close()
-		if err != nil {
-			log.Fatalf("awecron fatal (%s) {%s}: problem closing tmr file cjTmr", curUser.Username, path.Base(*cjDir))
-		}
-		// set the next run time as last modification time
-		err = os.Chtimes(*cjDir+"/tmr", time.Time{}, time.Unix(time.Now().Unix()+int64(cjCfg), int64(0)))
-		if err != nil {
-			log.Fatalf("awecron fatal (%s) {%s}: problem setting last modification time of tmr file", curUser.Username, path.Base(*cjDir))
-		}
+		// run the scheduling function
+		// no need to rerun user.Current()
+		scheduleCj(cjDir, &curUser.Username)
 	} else {
 		// log exit status
 		log.Printf("awecron error (%s) {%s} [%d]: cronjob run returned an error", curUser.Username, path.Base(*cjDir), cjCmd.ProcessState.ExitCode())
@@ -136,6 +104,45 @@ func runCj(cjDir *string) {
 		if cjStderr.String() != "" {
 			log.Printf("awecron info (%s) {%s}: cronjob run stderr output:\n==========\n%s\n==========", curUser.Username, path.Base(*cjDir), cjStderr.String())
 		}
+	}
+}
+
+// schedule the next run of the cronjob
+func scheduleCj(cjDir *string, curUser *string) {
+	// getting the plain text interval configuration
+	// its also possible to do it with fmt.Fscanf, but I've chosen this option
+	cjCfgData, err := os.ReadFile(*cjDir + "/cfg")
+	if err != nil {
+		log.Printf("awecron error (%s) {%s}: problem reading cronjob config file cjDir/cfg and saving as cronjob config data cjCfgData", *curUser, path.Base(*cjDir))
+		return
+	}
+	// conversion
+	cjCfg, err := strconv.Atoi(strings.TrimSpace(string(cjCfgData)))
+	if err != nil {
+		log.Printf("awecron error (%s) {%s}: problem converting cronjob config data cjCfgData into cronjob config integer cjCfg", *curUser, path.Base(*cjDir))
+		return
+	}
+	// make sure its greater than zero
+	if cjCfg <= 0 {
+		log.Printf("awecron error (%s) {%s}: cronjob config cjCfg should be greater than zero", *curUser, path.Base(*cjDir))
+		return
+	}
+	// create tmr file again
+	cjTmr, err := os.Create(*cjDir + "/tmr")
+	// all fatal errors because I am not risking with tmr file
+	// because it might result in an infinite loop for whatever reason
+	if err != nil {
+		log.Fatalf("awecron fatal (%s) {%s}: problem creating cjDir/tmr file", *curUser, path.Base(*cjDir))
+	}
+	// closing cjTmr file
+	err = cjTmr.Close()
+	if err != nil {
+		log.Fatalf("awecron fatal (%s) {%s}: problem closing tmr file cjTmr", *curUser, path.Base(*cjDir))
+	}
+	// set the next run time as last modification time
+	err = os.Chtimes(*cjDir+"/tmr", time.Time{}, time.Unix(time.Now().Unix()+int64(cjCfg), int64(0)))
+	if err != nil {
+		log.Fatalf("awecron fatal (%s) {%s}: problem setting last modification time of tmr file", *curUser, path.Base(*cjDir))
 	}
 }
 
