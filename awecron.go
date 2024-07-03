8aecd464a523ed915d5eee7c3e9b1d4d4ba30041
life00 <contact.5lu1v@simplelogin.co>
2024-07-03 11:23:44 +0200

    move scheduleCj() call to main() and curUser variable changes I wan't to get curUser only when necessary. Only if something fails. This adds more lines of code, but it won't create useless runtime variables.

--- awecron.go
+++ awecron.go
@@ -73,12 +73,11 @@ func checkCj(cjDir *string) bool {
 }
 
 // run the cronjob
-func runCj(cjDir *string) {
-	// used later for logging
-	curUser, _ := user.Current()
+func runCj(cjDir *string) bool {
 	// remove tmr file to disable cronjob in case of errors
 	err := os.Remove(*cjDir + "/tmr")
 	if err != nil {
+		curUser, _ := user.Current()
 		// fatal error because if it fails to disable the cronjob due to a problem then there may be an infinite loop
 		log.Fatalf("awecron fatal (%s) {%s}: problem deleting cjDir/tmr file", curUser.Username, path.Base(*cjDir))
 	}
@@ -92,39 +91,43 @@ func runCj(cjDir *string) {
 	err = cjCmd.Run()
 	// if successful run
 	if err == nil {
+		curUser, _ := user.Current()
 		// log everything
 		log.Printf("awecron info (%s) {%s} [%d]: cronjob run is successful", curUser.Username, path.Base(*cjDir), cjCmd.ProcessState.ExitCode())
-		// run the scheduling function
-		// no need to rerun user.Current()
-		scheduleCj(cjDir, &curUser.Username)
+		return true
 	} else {
+		curUser, _ := user.Current()
 		// log exit status
 		log.Printf("awecron error (%s) {%s} [%d]: cronjob run returned an error", curUser.Username, path.Base(*cjDir), cjCmd.ProcessState.ExitCode())
 		// log stderr if it is not empty
 		if cjStderr.String() != "" {
 			log.Printf("awecron info (%s) {%s}: cronjob run stderr output:\n==========\n%s\n==========", curUser.Username, path.Base(*cjDir), cjStderr.String())
 		}
+		return false
 	}
 }
 
 // schedule the next run of the cronjob
-func scheduleCj(cjDir *string, curUser *string) {
+func scheduleCj(cjDir *string) {
 	// getting the plain text interval configuration
 	// its also possible to do it with fmt.Fscanf, but I've chosen this option
 	cjCfgData, err := os.ReadFile(*cjDir + "/cfg")
 	if err != nil {
-		log.Printf("awecron error (%s) {%s}: problem reading cronjob config file cjDir/cfg and saving as cronjob config data cjCfgData", *curUser, path.Base(*cjDir))
+		curUser, _ := user.Current()
+		log.Printf("awecron error (%s) {%s}: problem reading cronjob config file cjDir/cfg and saving as cronjob config data cjCfgData", curUser.Username, path.Base(*cjDir))
 		return
 	}
 	// conversion
 	cjCfg, err := strconv.Atoi(strings.TrimSpace(string(cjCfgData)))
 	if err != nil {
-		log.Printf("awecron error (%s) {%s}: problem converting cronjob config data cjCfgData into cronjob config integer cjCfg", *curUser, path.Base(*cjDir))
+		curUser, _ := user.Current()
+		log.Printf("awecron error (%s) {%s}: problem converting cronjob config data cjCfgData into cronjob config integer cjCfg", curUser.Username, path.Base(*cjDir))
 		return
 	}
 	// make sure its greater than zero
 	if cjCfg <= 0 {
-		log.Printf("awecron error (%s) {%s}: cronjob config cjCfg should be greater than zero", *curUser, path.Base(*cjDir))
+		curUser, _ := user.Current()
+		log.Printf("awecron error (%s) {%s}: cronjob config cjCfg should be greater than zero", curUser.Username, path.Base(*cjDir))
 		return
 	}
 	// create tmr file again
@@ -132,17 +135,20 @@ func scheduleCj(cjDir *string, curUser *string) {
 	// all fatal errors because I am not risking with tmr file
 	// because it might result in an infinite loop for whatever reason
 	if err != nil {
-		log.Fatalf("awecron fatal (%s) {%s}: problem creating cjDir/tmr file", *curUser, path.Base(*cjDir))
+		curUser, _ := user.Current()
+		log.Fatalf("awecron fatal (%s) {%s}: problem creating cjDir/tmr file", curUser.Username, path.Base(*cjDir))
 	}
 	// closing cjTmr file
 	err = cjTmr.Close()
 	if err != nil {
-		log.Fatalf("awecron fatal (%s) {%s}: problem closing tmr file cjTmr", *curUser, path.Base(*cjDir))
+		curUser, _ := user.Current()
+		log.Fatalf("awecron fatal (%s) {%s}: problem closing tmr file cjTmr", curUser.Username, path.Base(*cjDir))
 	}
 	// set the next run time as last modification time
 	err = os.Chtimes(*cjDir+"/tmr", time.Time{}, time.Unix(time.Now().Unix()+int64(cjCfg), int64(0)))
 	if err != nil {
-		log.Fatalf("awecron fatal (%s) {%s}: problem setting last modification time of tmr file", *curUser, path.Base(*cjDir))
+		curUser, _ := user.Current()
+		log.Fatalf("awecron fatal (%s) {%s}: problem setting last modification time of tmr file", curUser.Username, path.Base(*cjDir))
 	}
 }
 
@@ -163,7 +169,11 @@ func main() {
 	for d := 0; d < len(cjDirs); d++ {
 		// TODO: implement timeout here, or for cjCmd
 		if checkCj(&cjDirs[d]) {
-			runCj(&cjDirs[d])
+			// if cronjob run is successful
+			if runCj(&cjDirs[d]) {
+				// schedule for next run
+				scheduleCj(&cjDirs[d])
+			}
 		}
 		// TODO: dynamic sleep here
 	}
