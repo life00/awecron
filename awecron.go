33c33c862e53a86e36a1e6284d7fd0d472681ff3
life00 <contact.5lu1v@simplelogin.co>
2024-07-21 11:42:53 +0200

    rewrite awecron in OOP Go

--- awecron.go
+++ awecron.go
@@ -18,11 +18,26 @@ import (
 	"github.com/pelletier/go-toml/v2"
 )
 
+// cron struct
+type Cron struct {
+	CfgDir      string
+	CjDirs      []string
+	CjSchedules []int
+	Cfg
+}
+
 // global awecron config type
-type cfgType struct {
+type Cfg struct {
 	Max     int
 	Min     int
 	Timeout int
+}
+
+// cronjob struct
+type Cronjob struct {
+	*Cron
+	Dir      string
+	Schedule int
 }
 
 // sets the logging format
@@ -42,14 +57,15 @@ func setLog() {
 
 // gets global configuration directory path
 // HACK: this function may need improvements
-func getCfgDir() string {
+func (c *Cron) getCfgDir() {
 	// config in $XDG_CONFIG_DIR/awecron or $HOME/.config/awecron
 	// get user config directory, check if file/directory exists, check if its a directory
 	if userCfgDir, err := os.UserConfigDir(); err == nil {
 		if cfgDirInfo, err := os.Stat(userCfgDir + "/awecron"); err == nil {
 			if cfgDirInfo.IsDir() {
 				// return if successful
-				return userCfgDir + "/awecron"
+				c.CfgDir = userCfgDir + "/awecron"
+				return
 			} else {
 				log.Fatalf("[FATAL]: global config directory %s is not a directory", userCfgDir+"/awecron")
 			}
@@ -60,76 +76,78 @@ func getCfgDir() string {
 	if cfgDirInfo, err := os.Stat("/etc/awecron"); err == nil {
 		if cfgDirInfo.IsDir() {
 			// return if successful
-			return "/etc/awecron"
+			c.CfgDir = "/etc/awecron"
+			return
 		} else {
 			log.Fatalf("[FATAL]: global config directory %s is not a directory", "/etc/awecron")
 		}
 	}
 	// could not find any matching directories
 	log.Fatalf("[FATAL]: global config directory does not exist")
-	return ""
 }
 
 // gets global awecron configuration
-func getCfg(cfgDir *string, cfg *cfgType) {
-	cfgData, err := os.ReadFile(*cfgDir + "/cfg")
+func (c *Cron) getCfg() {
+	cfgData, err := os.ReadFile(c.CfgDir + "/cfg")
 	if err != nil {
-		log.Fatalf("[FATAL]: problem reading global config file cfgDir/cfg and saving as global config data cfgData")
+		log.Fatalf("[FATAL]: problem reading global config file CfgDir/cfg and saving as global config data cfgData")
 	}
-	err = toml.Unmarshal(cfgData, cfg)
+	err = toml.Unmarshal(cfgData, &c.Cfg)
 	if err != nil {
 		log.Fatalf("[FATAL]: problem unmarshalling global config data cfgData as struct cfg{}")
 	}
-	if cfg.Max <= 0 || cfg.Min <= 0 || cfg.Timeout <= 0 {
+	if c.Max <= 0 || c.Min <= 0 || c.Timeout <= 0 {
 		log.Fatalf("[FATAL]: global config values cfg{} should be greater than zero")
 	}
 }
 
 // gets cronjob directory paths
-func getCjDirs(cfgDir *string) (cjDirs []string) {
-	cjTmrs, err := filepath.Glob(*cfgDir + "/*/tmr")
+func (c *Cron) getCjDirs() {
+	cjTmrs, err := filepath.Glob(c.CfgDir + "/*/tmr")
 	if err != nil {
-		log.Fatalf("[FATAL]: problem matching cfgDir/*/tmr and getting an array of cronjob timers cjTmrs")
+		log.Fatalf("[FATAL]: problem matching CfgDir/*/tmr and getting an array of cronjob timers cjTmrs")
 	}
 	// removing the /tmr end
 	for t := 0; t < len(cjTmrs); t++ {
-		cjDirs = append(cjDirs, strings.TrimSuffix(cjTmrs[t], "/tmr"))
+		c.CjDirs = append(c.CjDirs, strings.TrimSuffix(cjTmrs[t], "/tmr"))
 	}
-	return cjDirs
 }
 
 // check if its time to run the cronjob
-func checkCj(cjDir *string) (bool, int) {
+func (cj *Cronjob) check() bool {
 	// getting last modification date of tmr file
-	cjTmrInfo, err := os.Stat(*cjDir + "/tmr")
+	cjTmrInfo, err := os.Stat(cj.Dir + "/tmr")
 	if err != nil {
-		log.Printf("[ERROR] {%s}: problem getting last modification date of cjDir/tmr file as file info cjTmrInfo", path.Base(*cjDir))
-		// the 0 returned for cjSchedule is fixed later in main()
-		// this also applies to all returns in runCj and scheduleCj
-		return false, 0
+		log.Printf("[ERROR] {%s}: problem getting last modification date of dir/tmr file as file info cjTmrInfo", path.Base(cj.Dir))
+		// the 0 returned for schedule is fixed later in main()
+		// this also applies to all returns in run and schedule
+		cj.Schedule = 0
+		return false
 	}
-	cjSchedule := cjTmrInfo.ModTime().Unix()
+	schedule := cjTmrInfo.ModTime().Unix()
 	// check if its time to run the cronjob
-	if cjSchedule < time.Now().Unix() {
-		return true, 0
+	if schedule < time.Now().Unix() {
+		cj.Schedule = 0
+		return true
 	} else {
-		return false, int(cjSchedule)
+		cj.Schedule = int(schedule)
+		return false
 	}
 }
 
 // run the cronjob
-func runCj(cjDir *string, cjTimeout *int) bool {
+func (cj *Cronjob) run() bool {
 	// remove tmr file to disable cronjob in case of errors
-	err := os.Remove(*cjDir + "/tmr")
+	err := os.Remove(cj.Dir + "/tmr")
 	if err != nil {
 		// fatal error because if it fails to disable the cronjob due to a problem then there may be an infinite loop
-		log.Fatalf("[FATAL] {%s}: problem deleting cjDir/tmr file", path.Base(*cjDir))
+		log.Fatalf("[FATAL] {%s}: problem deleting dir/tmr file", path.Base(cj.Dir))
 	}
 	// declaring context timeout
-	cjCtx, cjCtxCancel := context.WithTimeout(context.Background(), time.Duration(*cjTimeout)*time.Second)
+	cjCtx, cjCtxCancel := context.WithTimeout(context.Background(), time.Duration(cj.Timeout)*time.Second)
 	defer cjCtxCancel()
 	// creating the cmd struct with context timeout
-	cjCmd := exec.CommandContext(cjCtx, *cjDir+"/run")
+	cjCmd := exec.CommandContext(cjCtx, cj.Dir+"/run")
 	// modifying function which will be used to stop the cronjob if it times out
 	// so that it contains the log message that cronjob has timed out
 	cjCmd.Cancel = func() (err error) {
@@ -139,11 +157,11 @@ func runCj(cjDir *string, cjTimeout *int) bool {
 			// non fatal error because if cjCmd.Process.Kill() will fail to stop the process
 			// cjCmd.Run() will exit and forward this error, which will say that cronjob returned an error
 			// so it won't reenable the cronjob and there is no persistent problem
-			log.Printf("[ERROR] {%s}: failed to stop the timed out cronjob", path.Base(*cjDir))
+			log.Printf("[ERROR] {%s}: failed to stop the timed out cronjob", path.Base(cj.Dir))
 			return err
 		}
 		// log that the cronjob has timed out
-		log.Printf("[INFO] {%s}: cronjob run has timed out, stopping", path.Base(*cjDir))
+		log.Printf("[INFO] {%s}: cronjob run has timed out, stopping", path.Base(cj.Dir))
 		return nil
 	}
 	// recording stderr
@@ -155,82 +173,85 @@ func runCj(cjDir *string, cjTimeout *int) bool {
 	// if successful run
 	if err == nil {
 		// log everything
-		log.Printf("[INFO] {%s} [%d]: cronjob run is successful", path.Base(*cjDir), cjCmd.ProcessState.ExitCode())
+		log.Printf("[INFO] {%s} [%d]: cronjob run is successful", path.Base(cj.Dir), cjCmd.ProcessState.ExitCode())
 		return true
 	} else {
 		// log exit status
-		log.Printf("[ERROR] {%s} [%d]: cronjob run returned an error", path.Base(*cjDir), cjCmd.ProcessState.ExitCode())
+		log.Printf("[ERROR] {%s} [%d]: cronjob run returned an error", path.Base(cj.Dir), cjCmd.ProcessState.ExitCode())
 		// log stderr if it is not empty
 		if cjStderr.String() != "" {
-			log.Printf("[INFO] {%s}: cronjob run stderr output:\n==========\n%s\n==========", path.Base(*cjDir), cjStderr.String())
+			log.Printf("[INFO] {%s}: cronjob run stderr output:\n==========\n%s\n==========", path.Base(cj.Dir), cjStderr.String())
 		}
 		return false
 	}
 }
 
 // schedule the next run of the cronjob
-func scheduleCj(cjDir *string) int {
+func (cj *Cronjob) getSchedule() {
 	// getting the plain text interval configuration
 	// its also possible to do it with fmt.Fscanf, but I've chosen this option
-	cjCfgData, err := os.ReadFile(*cjDir + "/cfg")
+	cjCfgData, err := os.ReadFile(cj.Dir + "/cfg")
 	if err != nil {
-		log.Printf("[ERROR] {%s}: problem reading cronjob config file cjDir/cfg and saving as cronjob config data cjCfgData", path.Base(*cjDir))
-		return 0
+		log.Printf("[ERROR] {%s}: problem reading cronjob config file dir/cfg and saving as cronjob config data cjCfgData", path.Base(cj.Dir))
+		cj.Schedule = 0
+		return
 	}
 	// conversion
 	cjCfg, err := strconv.Atoi(strings.TrimSpace(string(cjCfgData)))
 	if err != nil {
-		log.Printf("[ERROR] {%s}: problem converting cronjob config data cjCfgData into cronjob config integer cjCfg", path.Base(*cjDir))
-		return 0
+		log.Printf("[ERROR] {%s}: problem converting cronjob config data cjCfgData into cronjob config integer cjCfg", path.Base(cj.Dir))
+		cj.Schedule = 0
+		return
 	}
 	// make sure its greater than zero
 	if cjCfg <= 0 {
-		log.Printf("[ERROR] {%s}: cronjob config cjCfg should be greater than zero", path.Base(*cjDir))
-		return 0
+		log.Printf("[ERROR] {%s}: cronjob config cjCfg should be greater than zero", path.Base(cj.Dir))
+		cj.Schedule = 0
+		return
 	}
 	// create tmr file again
-	cjTmr, err := os.Create(*cjDir + "/tmr")
+	cjTmr, err := os.Create(cj.Dir + "/tmr")
 	// all fatal errors because I am not risking with tmr file
 	// because it might result in an infinite loop for whatever reason
 	if err != nil {
-		log.Fatalf("[FATAL] {%s}: problem creating cjDir/tmr file", path.Base(*cjDir))
+		log.Fatalf("[FATAL] {%s}: problem creating dir/tmr file", path.Base(cj.Dir))
 	}
 	// closing cjTmr file
 	err = cjTmr.Close()
 	if err != nil {
-		log.Fatalf("[FATAL] {%s}: problem closing tmr file cjTmr", path.Base(*cjDir))
+		log.Fatalf("[FATAL] {%s}: problem closing tmr file cjTmr", path.Base(cj.Dir))
 	}
 	// get next run time
-	cjSchedule := time.Now().Unix() + int64(cjCfg)
+	schedule := time.Now().Unix() + int64(cjCfg)
 	// set the next run time as last modification time
-	err = os.Chtimes(*cjDir+"/tmr", time.Time{}, time.Unix(cjSchedule, int64(0)))
+	err = os.Chtimes(cj.Dir+"/tmr", time.Time{}, time.Unix(schedule, int64(0)))
 	if err != nil {
-		log.Fatalf("[FATAL] {%s}: problem setting last modification time of tmr file", path.Base(*cjDir))
+		log.Fatalf("[FATAL] {%s}: problem setting last modification time of tmr file", path.Base(cj.Dir))
 	}
-	return int(cjSchedule)
+	cj.Schedule = int(schedule)
 }
 
 // get optimal sleep time until next cronjob
-func getSleepTime(cjSchedules *[]int, cfg *cfgType) (sleepTime int) {
+func (c Cron) getSleepTime() (sleepTime int) {
 	// if there is no cronjobs sleep max time
-	if len(*cjSchedules) == 0 {
+	if len(c.CjSchedules) == 0 {
 		log.Printf("[INFO]: no enabled cronjobs found, sleeping max time")
-		return cfg.Max
+		return c.Max
 	}
 	// get the smallest unix time stamp from cronjob schedules
-	minCjSchedule := (*cjSchedules)[0]
-	for _, cjSchedule := range (*cjSchedules)[1:] {
-		if cjSchedule < minCjSchedule {
-			minCjSchedule = cjSchedule
+	minCjSchedule := c.CjSchedules[0]
+	for _, schedule := range c.CjSchedules[1:] {
+		if schedule < minCjSchedule {
+			minCjSchedule = schedule
 		}
 	}
 	// get the sleep time
 	sleepTime = minCjSchedule - int(time.Now().Unix())
 	// apply the limits
-	if sleepTime < cfg.Min {
-		sleepTime = cfg.Min
-	} else if sleepTime > cfg.Max {
-		sleepTime = cfg.Max
+	if sleepTime < c.Min {
+		sleepTime = c.Min
+	} else if sleepTime > c.Max {
+		sleepTime = c.Max
 	}
 	// return the optimal sleep time
 	return sleepTime
@@ -240,23 +261,22 @@ func main() {
 	// setting the logging format
 	setLog()
 	// getting the config directory
-	cfgDir := getCfgDir()
-	// global awecron config
-	var cfg cfgType
+	var c Cron
+	c.getCfgDir()
+	c.CfgDir = "/tmp/awecron"
 	// getting global awecron configuration
-	getCfg(&cfgDir, &cfg)
+	c.getCfg()
 	// infinite loop
 	for {
 		// getting cronjob directories
-		cjDirs := getCjDirs(&cfgDir)
-		// array of unix time stamps until next cronjob run
-		var cjSchedules []int
+		c.getCjDirs()
 		// create mutex for managing above array inside of goroutines
 		var cjMutex sync.Mutex
 		// create wait group for goroutines
 		var cjWG sync.WaitGroup
-		// loop through cjDirs
-		for _, cjDir := range cjDirs {
+		// loop through CjDirs
+		for i := range c.CjDirs {
+			cj := Cronjob{&c, c.CjDirs[i], 0}
 			// add one goroutine to wait group
 			cjWG.Add(1)
 			// initialize goroutine
@@ -264,30 +284,32 @@ func main() {
 				defer cjWG.Done()
 				// in awecron.sh I run a separate function for dynamic sleep feature to determine how much for awecron to sleep, which is pretty inefficient
 				// here instead I will make existing functions return necessary values
-				var cjSchedule int
-				var checkCjReturn bool
 				// check if its necessary to run the cronjob
-				if checkCjReturn, cjSchedule = checkCj(&cjDir); checkCjReturn {
+				if cj.check() {
 					// run the cronjob
-					if runCj(&cjDir, &cfg.Timeout) {
+					if cj.run() {
 						// schedule the cronjob for next run
-						cjSchedule = scheduleCj(&cjDir)
+						cj.getSchedule()
 					}
 				}
-				// if the function fails it has to return something as cjSchedule, so it returns 0
+				// if the function fails it has to return something as schedule, so it returns 0
 				// so if its 0 it won't add it to the array of schedules at all
-				if cjSchedule != 0 {
-					// mutex lock while appending cjSchedule to an array, then unlock
+				if cj.Schedule != 0 {
+					// mutex lock while appending schedule to an array, then unlock
 					cjMutex.Lock()
 					// append the next run time to the array of schedules
-					cjSchedules = append(cjSchedules, cjSchedule)
+					c.CjSchedules = append(c.CjSchedules, cj.Schedule)
 					cjMutex.Unlock()
 				}
 			}()
 		}
 		// wait until all cronjobs finish
 		cjWG.Wait()
+		// wipe CjDirs array
+		c.CjDirs = nil
 		// get optimal sleep time and sleep for that number of seconds
-		time.Sleep(time.Duration(getSleepTime(&cjSchedules, &cfg)) * time.Second)
+		time.Sleep(time.Duration(c.getSleepTime()) * time.Second)
+		// wipe CjSchedules array
+		c.CjSchedules = nil
 	}
 }
